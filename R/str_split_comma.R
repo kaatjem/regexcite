2b8e3cd0037e567e31180fe07afda7e746c7c6ad
KaatjeM <kaatje.matthews@gmail.com>
2026-05-18 19:50:35 -0700

    added roxygen comments

--- R/str_split_comma.R
+++ R/str_split_comma.R
@@ -1,3 +1,12 @@
+#' Split a string on commas
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @returns A character vector.
+#' @export
+#'
+#' @examples
 str_split_comma <- function(string) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
--- R/str_split_nums.R
+++ R/str_split_nums.R
@@ -1,7 +1,17 @@
+#' Split a string and convert to numeric
+#'
+#' @param string A character vector with, at most, one element.
+#' @param pattern A
+#' @inheritParams stringr::str_split
+#'
+#' @returns A character vector.
+#' @export
+#'
+#' @examples
 str_split_nums <- function(string, pattern){
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
-    as.integer(stringr::str_split(string = string, pattern = pattern)[[1]])
+    as.numeric(stringr::str_split(string = string, pattern = pattern)[[1]])
   } else {
     character()
   }
